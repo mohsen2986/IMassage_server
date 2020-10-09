ed8cc65d5f3d9e00e530f876634f425c68058c0e
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-10-09 21:25:40 +0330

    add Responser for api and ApiController

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use ApiResponser;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    use ApiResponser;
+
+}
---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,29 @@
+<?php
+
+trait ApiResponser
+{
+    private function successResponse($data, $code)
+    {
+        return response()->json($data, $code);
+    }
+
+    protected function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    protected function showAll(Collection $collection, $code = 200)
+    {
+        if ($collection->isEmpty()) {
+            return $this->successResponse(['data' => $collection], $code);
+        }
+        return $this->successResponse($collection, $code);
+    }
+
+    protected function showOne(Model $instance, $code = 200)
+    {
+        return $this->successResponse($instance, $code);
+    }
+
+}
+
