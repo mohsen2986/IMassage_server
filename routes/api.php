f73074f42c6b4ae41b9bd613cb5b84d01e5665c1
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-10-20 22:59:47 +0330

    add loginController.php and LoginVerificationController.php and RegisterController.php and RegisterController.php to the routes

--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::resource('packages' , 'Packages\PackagesController' , ['except' => ['create' ,  'edit']]);
 Route::resource('questionType' , 'Question\QuestionTypeController' , ['only' => ['index' , 'store' , 'destroy']]);
+// login user
+Route::resource('login' , 'Auth\LoginController' , ['only' => ['store']]);
+Route::resource('loginVerify' , 'Auth\LoginVerificationController' , ['only' => ['store']]);
+// register user
+Route::resource('register' , 'Auth\RegisterController' , ['only' => ['store']]);
+Route::resource('registerVerify' , 'Auth\RegisterVerificationController' , ['only' => ['store']]);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
