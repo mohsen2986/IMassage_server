ff7f7af0ee831c428924ae5e243601588973af12
mohsen <mohsenaliakbari.atom@hotmail.com>
2020-11-06 05:13:11 +0330

    update offer controller

--- app/Http/Controllers/Offer/OfferController.php
+++ app/Http/Controllers/Offer/OfferController.php
@@ -4,83 +4,69 @@ namespace App\Http\Controllers\Offer;
 
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
+use App\Offers;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
+use Illuminate\Validation\ValidationException;
 
 class OfferController extends ApiController
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return $this->showAll(Offers::all());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
      */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+        $rules =[
+            'number' => 'required',
+            'percent' => 'required'
+        ];
+        $this->validate($request , $rules);
+        // generate data
+        // hold offers for response
+        $offers = new Collection();
+        for($i=0 ; $i<request('number'); $i++){
+            // set data
+            $data['code'] = Offers::generateCode();
+            $temp = explode(' ', jdate());
+            $data['date'] = $temp[0];
+            $data['validate'] = Offers::VALIDATE;
+            $data['offer'] = request('percent');
+            // store offer
+            $offers->push(Offers::create($data));
+            // add it to the response
+        }
+        return $this->showAll($offers);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Offers $offer
+     * @return JsonResponse
+     * @throws \Exception
      */
-    public function destroy($id)
+    public function destroy(Offers $offer)
     {
-        //
+        // delete the offer from the database
+        $offer->delete();
+
+        return $this->showOne($offer);
     }
 }
--- app/Offers.php
+++ app/Offers.php
@@ -3,7 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
+/**
+ * Class Massage
+ * @package App
+ * @property mixed code
+ * @property mixed date
+ * @property mixed validate
+ * @property mixed offer
+ */
 class Offers extends Model
 {
     const VALIDATE = '1';
@@ -18,10 +27,15 @@ class Offers extends Model
         'validate' ,
         'offer' ,
     ];
+    // relations
     public function order(){
         return $this->hasMany(Order::class);
     }
     public function usedOffers(){
         return $this->belongsTo(UsedOffers::class);
+    }
+    // functions
+    public static function generateCode(){
+        return Str::random(5);
     }
 }
